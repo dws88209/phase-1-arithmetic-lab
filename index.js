90472c4716ab23eb0a34783fac81d8a75967a108
Destinee Saunders <dsaunders290@gmail.com>
2022-09-06 10:28:22 -0400

    commit tree a70c35fcf108867f9adebcaa768aae50f01eaeb5

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 2
+const num2 = 31
+let multiply= num1*num2
 
-// Write your code here
+let random= Math.floor(Math.random()*10) +1;
+
+const num3 = 28
+const num4 = 8
+let mod= num3%num4
+
+let max= Math.max(2,5,12,20)
